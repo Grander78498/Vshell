032b0c1a406eda4fa338cf85d465a4abc9d58948
Grander78498 <grander.l.l.1242@gmail.com>
2023-09-15 23:12:03 +0300

    Not last commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tarfile
 import sys
 import cmd
 import os
+import argparse
 
 
 class MyCmd(cmd.Cmd):
@@ -13,6 +14,8 @@ class MyCmd(cmd.Cmd):
 
     def __init__(self):
         super().__init__()
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--script", )
         filename = sys.argv[1]
         if os.path.exists(filename) and not os.path.isdir(filename) and tarfile.is_tarfile(filename):
             self.file = tarfile.TarFile(filename)
@@ -23,54 +26,91 @@ class MyCmd(cmd.Cmd):
             sys.exit(0)
 
     def do_exit(self, arg):
-        if self.file:
-            self.file.close()
-            self.file = None
-        return True
+        if "--help" in arg:
+            print("""
+                  exit: exit
+                        Выполняет выход из эмулятора vshell.
+                  """)
+        else:
+            if self.file:
+                self.file.close()
+                self.file = None
+            return True
 
     def do_ls(self, arg):
-        if arg == "":
-            full_path = self.current_directory
+        if "--help" in arg:
+            print("""
+                  ls: ls [ФАЙЛ]
+                        Выводит информацию о файлах (по умолчанию в текущем каталоге).
+                  """)
         else:
-            full_path = self.get_fullpath(arg)
-        matches = []
-        for elem in self.file.getnames():
-            if full_path in elem and full_path != elem:
-                matches.append(elem.replace(full_path, "").split("/")[1])
-        matches = list(set(matches))
-        print(" ".join([elem.split("/")[0] for elem in matches]))
+            if arg == "":
+                full_path = self.current_directory
+            else:
+                full_path = self.get_fullpath(arg)
+            matches = []
+            for elem in self.file.getnames():
+                if full_path in elem and full_path != elem:
+                    matches.append(elem.replace(full_path, "").split("/")[1])
+            matches = list(set(matches))
+            print(" ".join([elem.split("/")[0] for elem in matches]))
 
     def do_cd(self, arg):
-        full_path = self.get_fullpath(arg)
-
-        if full_path in self.file.getnames():
-            self.current_directory = full_path
-            self.prompt = f"user: {full_path.replace('root', '/').replace('//', '/')}$ "
+        if "--help" in arg:
+            print("""
+                  cd: cd [ФАЙЛ]
+                        Меняет текущую директорию (по умолчанию, на текущую директорию).
+                  """)
         else:
-            print(f"vshell: cd: {full_path}: Нет такого файла или каталога")
+            full_path = self.get_fullpath(arg)
+
+            if full_path in self.file.getnames():
+                if self.file.extractfile(full_path) is None:
+                    self.current_directory = full_path
+                    self.prompt = f"user: {full_path.replace('root', '/').replace('//', '/')}$ "
+                else:
+                    print(f"vshell: cd: {full_path}: Это не каталог")
+            else:
+                print(f"vshell: cd: {full_path}: Нет такого файла или каталога")
 
     def do_pwd(self, arg):
-        print(self.current_directory.replace('root', '/').replace('//', '/'))
+        if "--help" in arg:
+            print("""
+                  pwd: pwd
+                        Печатает текущую директорию.
+                  """)
+        else:
+            print(self.current_directory.replace('root', '/').replace('//', '/'))
 
     def do_cat(self, arg):
-        paths = [self.get_fullpath(elem) for elem in arg.split()]
-        content = ""
-        for path in paths:
-            if path in self.file.getnames():
-                try:
-                    read_file = self.file.extractfile(path)
-                    content += read_file.read().decode()
-                    read_file.close()
-                except:
-                    print("vshell: cat: Это каталог")
-            else:
-                print(f"vshell: cat: {path}: Нет такого файла или каталога")
+        if "--help" in arg:
+            print("""
+                  cat: cat [ФАЙЛ(-ы)]
+                        Печатает слияние ФАЙЛ(-ов) в стандартный вывод.
+                  """)
+        else:
+            paths = [self.get_fullpath(elem) for elem in arg.split()]
+            content = ""
+            for path in paths:
+                if path in self.file.getnames():
+                    try:
+                        read_file = self.file.extractfile(path)
+                        content += read_file.read().decode()
+                        read_file.close()
+                    except:
+                        print("vshell: cat: Это каталог")
+                else:
+                    print(f"vshell: cat: {path}: Нет такого файла или каталога")
+            print(content.rstrip())
 
     def get_fullpath(self, arg):
         if arg[0:2] == "..":
             full_path = arg.replace("..", "/".join(self.current_directory.split('/')[:-1]))
         elif arg[0] == "/":
-            full_path = self.root_directory + arg
+            if arg == "/":
+                full_path = self.root_directory
+            else:
+                full_path = self.root_directory + arg
         else:
             full_path = self.current_directory + "/" + arg
 
